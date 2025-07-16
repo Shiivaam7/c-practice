79a1009aa60515bda173287a9bc909baf5927fe8
shivam kumar <shivamraj0k0r@gmail.com>
2025-07-16 14:20:53 +0530

    second file added

---
+++ practice2.cpp
@@ -0,0 +1,10 @@
+//22. Print numbers from 50 to 100 using while loop.
+#include<iostream>
+using namespace std;
+int main(){
+    int num = 50;
+    while(num<=100){
+        cout<<" number is "<<num<<endl;
+        num = num + 1;
+    }
+}
