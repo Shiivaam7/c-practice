3f16ee10d38d9d622f321aa59bb46b539fab2a1f
shivam kumar <shivamraj0k0r@gmail.com>
2025-07-16 14:29:31 +0530

    file 3 added

---
+++ practice3.cpp
@@ -0,0 +1,10 @@
+//23. Print numbers from 30 down to 1 using while loop.
+#include<iostream>
+using namespace std;
+int main(){
+    int num = 30;
+    while(num>=1){
+        cout<<" number is "<<num<<endl;
+        num = num-1;
+    }
+}
