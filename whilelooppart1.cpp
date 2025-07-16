9c56b4692221a3a01055ea90c0acabf1d7730d8b
shivam kumar <shivamraj0k0r@gmail.com>
2025-07-16 14:11:20 +0530

    whilelooppart1 added

---
+++ whilelooppart1.cpp
@@ -0,0 +1,10 @@
+//Print numbers from 1 to 20 using while loop.
+#include<iostream>
+using namespace std;
+int main(){
+    int num = 1;
+    while(num<=20){
+        cout<<" number is "<<num<<endl;
+        num = num + 1;
+    }
+}
